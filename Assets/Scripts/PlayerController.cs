291834d528d0deb33326199dc535d8d4f8a6729b
Ket <ketcodesgames@gmail.com>
2025-10-07 22:06:14 -0700

    Add double jump and dash and after image effect

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,27 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float jumpForce = 10f;
+    [SerializeField] float dashSpeed = 25f;
+    [SerializeField] float dashDuration = 0.2f;
+    [SerializeField] float afterImageLifetime = 0.1f;
+    [SerializeField] float afterImageSpawnInterval = 0.05f;
+    [SerializeField] float dashCooldown = 1f;
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundLayer;
     [SerializeField] BulletController shot;
     [SerializeField] Transform shotPoint;
-    
+    [SerializeField] SpriteRenderer playerSpriteRenderer;
+    [SerializeField] SpriteRenderer afterImageSpriteRenderer;
+    [SerializeField] Color afterImageColor;
+
     Rigidbody2D playerRigidBody;
     Animator playerAnimator;
     Vector2 moveInput;
     bool isGrounded;
+    bool canDoubleJump;
+    float dashCounter = 0f;
+    float afterImageCounter = 0f;
+    float dashRechargeCounter = 0f;
 
     void Start()
     {
@@ -23,13 +35,45 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        Dash();
+        DashCooldown();
         Run();
         IsGrounded();
         FlipSprite();
     }
 
+    void Dash()
+    {
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+            playerRigidBody.linearVelocity = new Vector2(transform.localScale.x * dashSpeed, playerRigidBody.linearVelocity.y);
+            afterImageCounter -= Time.deltaTime;
+
+            if (afterImageCounter <= 0f)
+            {
+                ShowAfterImage();
+            }
+
+            dashRechargeCounter = dashCooldown;
+        }
+    }
+
+    void DashCooldown()
+    {
+        if (dashRechargeCounter > 0f)
+        {
+            dashRechargeCounter -= Time.deltaTime;
+        }
+    }   
+
     void Run()
     {
+        if (dashCounter > 0)
+        {
+            return;
+        }
+
         Vector2 moveVector = new Vector2(moveInput.x * moveSpeed, playerRigidBody.linearVelocity.y);
         playerRigidBody.linearVelocity = moveVector;
 
@@ -44,8 +88,19 @@ public class PlayerController : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-        if (value.isPressed && isGrounded)
+        if (value.isPressed &&
+            (isGrounded || canDoubleJump))
         {
+            if (isGrounded)
+            {
+                canDoubleJump = true;
+            }
+            else
+            {
+                canDoubleJump = false;
+                playerAnimator.SetTrigger("DoubleJump");
+            }
+
             playerRigidBody.linearVelocity = new Vector2(playerRigidBody.linearVelocity.x, jumpForce);
         }
     }
@@ -54,6 +109,15 @@ public class PlayerController : MonoBehaviour
     {
         Instantiate(shot, shotPoint.position, shotPoint.rotation).SetMoveDirection(transform.localScale.x, 0f);
         playerAnimator.SetTrigger("IsShooting");
+    }
+
+    void OnDash(InputValue value)
+    {
+        if (value.isPressed && dashCounter <= 0f && dashRechargeCounter <= 0f)
+        {
+            dashCounter = dashDuration;
+            ShowAfterImage();
+        }
     }
 
     void FlipSprite()
@@ -70,5 +134,17 @@ public class PlayerController : MonoBehaviour
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, .2f, groundLayer);
         playerAnimator.SetBool("IsJumping", !isGrounded);
+    }
+
+    void ShowAfterImage()
+    {
+        SpriteRenderer afterImage = Instantiate(afterImageSpriteRenderer, transform.position, transform.rotation);
+        afterImage.sprite = playerSpriteRenderer.sprite;
+        afterImage.transform.localScale = transform.localScale;
+        afterImage.color = afterImageColor;
+
+        Destroy(afterImage.gameObject, afterImageLifetime);
+
+        afterImageCounter = afterImageSpawnInterval;
     }
 }
