9b51fd1e5f0a7816a19ccfdaf12f2c4293498dc6
Ket <ketcodesgames@gmail.com>
2025-10-18 20:31:20 -0700

    Add destructible objects

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -4,6 +4,8 @@ public class BombController : MonoBehaviour
 {
     [SerializeField] private float explosionDelay = .5f;
     [SerializeField] private GameObject explosionEffect;
+    [SerializeField] private float bombBlastRadius = 1.5f;
+    [SerializeField] private LayerMask destructibleLayer;
 
     void Update()
     {
@@ -16,7 +18,24 @@ public class BombController : MonoBehaviour
                 Instantiate(explosionEffect, transform.position, Quaternion.identity);
             }
 
+            Collider2D[] hitObjects = Physics2D.OverlapCircleAll(transform.position, bombBlastRadius, destructibleLayer);
+            DestroyHitObjects(hitObjects);
+
             Destroy(gameObject, explosionDelay);
+        }
+    }
+    
+    void  DestroyHitObjects(Collider2D[] hitObjects)
+    {
+        if (hitObjects.Length > 0)
+        {
+            foreach (Collider2D hitObject in hitObjects)
+            {
+                if (hitObject != null)
+                {
+                    Destroy(hitObject.gameObject);
+                }
+            }
         }
     }
 }
